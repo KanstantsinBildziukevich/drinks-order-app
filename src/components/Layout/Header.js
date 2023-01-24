cdff3bd50a39b4f0782915e9560ce3c0d1d37f2d
Constantine <131313Constantine@gmail.com>
2023-01-24 03:15:34 +0300

    feat: header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import { Fragment } from 'react';
+import drinksImage from '../../assets/drinks.jpg'
+import classes from './Header.module.css';
+
+const Header = props => {
+  return <Fragment>
+    <header className={classes.header}>
+      <h1>Drinks</h1>
+      <button>Cart</button>
+    </header>
+    <div className={classes['main-image']}>
+      <img src={drinksImage} alt='wine bottle on the table'/>
+    </div>
+  </Fragment>
+};
+
+export default Header;
