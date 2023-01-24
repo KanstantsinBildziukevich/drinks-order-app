105ef2866c923d2179417962cb35d7c13ff4649e
Constantine <131313Constantine@gmail.com>
2023-01-24 16:22:59 +0300

    feat: cart and modals

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,15 +4,17 @@ import drinksImage from '../../assets/drinks.jpg'
 import classes from './Header.module.css';
 
 const Header = (props) => {
-  return <Fragment>
-    <header className={classes.header}>
-      <h1>Drinks</h1>
-      <HeaderCartButton />
-    </header>
-    <div className={classes['main-image']}>
-      <img src={drinksImage} alt='wine bottle on the table'/>
-    </div>
-  </Fragment>
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Drinks</h1>
+        <HeaderCartButton onClick={props.onShowCart}/>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={drinksImage} alt='wine bottle on the table'/>
+      </div>
+    </Fragment>
+  )
 };
 
 export default Header;
